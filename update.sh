d60aad710d88474ea69ca433d5df48caff00b29f
Greg Banas <banasg@gmail.com>
2016-11-03 10:58:33 +0000

    Remove submodules' and Maven entries from update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-git submodule foreach git pull origin master
-home=$PWD
-cd sdx-collect
-mvn clean package
-cd $home
-cd sdx-store
-mvn clean package
-cd $home
-eval $(docker-machine env)
