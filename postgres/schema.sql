98ef25f6eeb9ffc28cbe8cf732fe541770b48fee
Tejas Patel <tejas.patel@ons.gov.uk>
2017-07-19 10:04:20 +0100

    Changes to sql

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS responses (tx_id uuid PRIMARY KEY, ts timestamp WITH time zone DEFAULT NOW(), invalid boolean DEFAULT NULL, data jsonb);
-CREATE TABLE IF NOT EXISTS feedback_responses (id SERIAL, ts timestamp WITH time zone DEFAULT NOW(), invalid boolean DEFAULT NULL, data jsonb, survey char(25), period char(25));
+CREATE TABLE IF NOT EXISTS responses (tx_id uuid PRIMARY KEY, ts timestamp WITH time zone DEFAULT NOW(), valid boolean DEFAULT NULL, data jsonb);
+CREATE TABLE IF NOT EXISTS feedback_responses (id SERIAL PRIMARY KEY, ts timestamp WITH time zone DEFAULT NOW(), valid boolean DEFAULT NULL, data jsonb, survey char(25), period char(25));
 CREATE SEQUENCE sequence MINVALUE 1000 MAXVALUE 9999 CYCLE;
 CREATE SEQUENCE batch_sequence MINVALUE 30000 MAXVALUE 39999 CYCLE;
 CREATE SEQUENCE image_sequence MINVALUE 1 MAXVALUE 999999999 CYCLE;
